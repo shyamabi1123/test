1933c3d8cd831cebbaff0e6ecfa7a87b0dbb3248
shyamabi1123 <shyamabishek1@gmail.com>
2026-04-17 10:39:24 +0530

    Refactor methods in Test class to improve structure

--- Test.java
+++ Test.java
@@ -1,50 +1,37 @@
 public class Test {
 
-  static void add(){
-
-    int a=10;
-    int b=20;
-    int result = a+b;
-    System.out.println("The sum is " +result);
-    
-  }
-  static void sub(){
-
-    int c=20;
-    int d=10;
-    int result1 = c-d;
-    System.out.println("The diff is " +result1);
-    
-  }
-
-   static void multi(){
-
-    int x=5;
-    int y=8;
-    int result2 = x*y;
-    System.out.println("The multiplcation result is: " +result2);
-
-   }
-
-    static void div(){
-
-    int s=10;
-    int t=5;
-    int result3 = s/t;
-    System.out.println("The division result is: " +result3);
-
+    static void add() {
+        int a = 10;
+        int b = 20;
+        int result = a + b;
+        System.out.println("The sum is: " + result);
     }
-    
 
+    static void sub() {
+        int c = 20;
+        int d = 10;
+        int result1 = c - d;
+        System.out.println("The difference is: " + result1);
+    }
 
-  public static void main(String[] args)
-  {
-    add();
-    sub();
-    multi();
-    div();
+    static void multi() {
+        int x = 5;
+        int y = 8;
+        int result2 = x * y;
+        System.out.println("The multiplication result is: " + result2);
+    }
 
-  }
-   
-  }
+    static void div() {
+        int s = 10;
+        int t = 5;
+        int result3 = s / t;
+        System.out.println("The division result is: " + result3);
+    }
+
+    public static void main(String[] args) {
+        add();
+        sub();
+        multi();
+        div();
+    }
 }
